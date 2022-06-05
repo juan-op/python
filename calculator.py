a9103798a1d8422f589ee3341da7e1bfc43945e1
Juan Ochoa <106767807+juan-op@users.noreply.github.com>
2022-06-05 21:56:48 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def calculator():
+    while True:
+        print('** Calculator **')
+        a = int(input('First number: '))
+        c = input('Operation: ')
+        b = int(input('Second number: '))
+        if c == '+':
+            print(a + b)
+        elif c == '-':
+            print(a - b)
+        elif c == '*':
+            print(a * b)
+        elif c == '/':
+            try:
+                print(a / b)
+            except ZeroDivisionError:
+                print('Cannot divide by 0.')
+        elif c not in ('+', '-', '*', '/'):
+            print('Enter a valid operation.')
+        keep = input('Continue? Y/N: ').upper()
+        if keep != 'Y':
+            print('Exit.')
+            break
+
+
+calculator()
